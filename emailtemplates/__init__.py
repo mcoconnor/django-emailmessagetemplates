c1fb2b9114279e6b36ef567b6f7d939e0286234a
Michael O'Connor <michael@mcoconnor.net>
2015-03-18 13:39:02 -0500

    Add version designator

--- emailtemplates/__init__.py
+++ emailtemplates/__init__.py
@@ -5,5 +5,6 @@ Tools for sending templated emails
 from models import EmailMessageTemplate
 from utils import send_mail, send_mass_mail, mail_admins, mail_managers
 
+__version__ = '0.1.0'
 __all__ = [EmailMessageTemplate, send_mail,
            send_mass_mail, mail_admins, mail_managers]
