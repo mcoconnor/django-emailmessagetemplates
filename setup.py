7aea77c855885d20421bdb7c867ccd943b387531
Michael O'Connor <michael@mcoconnor.net>
2015-03-18 14:54:15 -0500

    Removed reference to module's version in setup.py to prevent import errors

--- setup.py
+++ setup.py
@@ -4,14 +4,13 @@
 import os
 import sys
 
-import emailmessagetemplates
 
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
-version = emailmessagetemplates.__version__
+version = "0.1.0"
 
 if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
