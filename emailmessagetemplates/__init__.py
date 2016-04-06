6eb2d2ee6577cf2aff979bd7c9e3527f929e600d
Michael O'Connor <michael@mcoconnor.net>
2016-04-06 13:53:49 -0500

    Add support for Django 1.9

--- emailmessagetemplates/__init__.py
+++ emailmessagetemplates/__init__.py
@@ -2,9 +2,4 @@
 Tools for sending templated emails
 """
 
-from models import EmailMessageTemplate
-from utils import send_mail, send_mass_mail, mail_admins, mail_managers
-
 __version__ = '0.1.1'
-__all__ = [EmailMessageTemplate, send_mail,
-           send_mass_mail, mail_admins, mail_managers]
